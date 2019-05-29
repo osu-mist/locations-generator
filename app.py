85b1a10d823fd96b5f4f03e0aa0a6ab69e5e82ad
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2019-05-29 10:52:02 -0700

    Load config, set logger

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import logging
 
+import requests
+
+import utils
+
+
+if __name__ == '__main__':
+    arguments = utils.parse_arguments()
+
+    # Setup logging level
+    if arguments.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    config = utils.load_config()
---
+++ utils.py
@@ -0,0 +1,35 @@
+import argparse
+import logging
+import sys
+
+import yaml
+
+
+def parse_arguments():
+    """
+    Handler function for parsing command-line arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--config',
+        dest='config_path',
+        help='Path to yaml formatted config file',
+        required=True)
+    parser.add_argument(
+        '--debug',
+        dest='debug',
+        help='Enable debug logging mode',
+        action='store_true')
+    return parser.parse_args()
+
+
+def load_config():
+    """
+    Handler function for loading config file
+    """
+    with open('configuration.yaml', 'r') as configuration:
+        try:
+            return yaml.safe_load(configuration)
+        except yaml.YAMLError as error:
+            logging.debug(error)
+            sys.exit('Unable to parse configuration.yaml')
