dfd8cb5908b0a606a18b265009162d0bc97602ff
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2019-08-29 13:34:29 -0700

    Fix bug if bad date format

--- utils.py
+++ utils.py
@@ -98,7 +98,7 @@ def format_library_hour(string):
     string = string.strip()
 
     # e.g. 7:00am -> 07:00am
-    return '0' + string if len(string) <= 7 else string
+    return '0' + string if 0 < len(string) <= 7 else string
 
 
 def to_utc(dt):
